617357e2c46a22d4adecd9529d7fac95bb396e5f
Muhammad Sabir <sabir.tabi1122@gmail.com>
2025-06-05 05:27:17 +0500

    Update and finalize data_loader.py with proper logging

--- src/pipeline/data_loader.py
+++ src/pipeline/data_loader.py
@@ -1,6 +1,8 @@
 import io
 import pandas as pd
 
+from src.logger.logger import logger
+
 def load_data(path):
     """
     Takes the location of the csv file and returns a dataframe of the csv file provided.
@@ -11,16 +13,34 @@ def load_data(path):
     Returns:
         df: Dataframe of the loaded csv
     """
-    df = pd.read_csv(path)
-    return df
+    try:
+        df = pd.read_csv(path)
+    except FileNotFoundError:
+        logger.error(f"The file at {path} was not found")
+    except Exception as err:
+        logger.error(f"An unexcected error occured while loading {path} file: {err}")
+    else:
+        return df
 
 def get_dataset_info(df):
     """
     Takes in a dataframe and returns its .info() in string.
+
+    Parameters:
+        df: A pandas DataFrame
+    
+    Returns:
+        info_string: df.info() converted into string
     """
-    buffer = io.StringIO()
-    df.info(buf=buffer)
-    info_string = buffer.getvalue()
-    buffer.close()
-    print("TYPE:", type(info_string))
+    try:
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("The parameter 'df' must be a pandas DataFrame")
+
+        buffer = io.StringIO()
+        df.info(buf=buffer)
+        info_string = buffer.getvalue()
+    except Exception as err:
+        logger.error(f"An unexpected error occured {err}")
+    finally:
+        buffer.close()
     return info_string
